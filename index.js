45ca20fedfad895d941d9392c997fd6d85af769f
Luke Bayes <lukebayes@gmail.com>
2016-12-13 15:50:12 -0800

    Fixed load

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ import {renderElement} from 'nomplate';
 
 
 if (require.main === module) {
-  console.log('LOOOOOOOOOOOOOOOOOOOOODED', global.document);
-  const element = renderElement(constitutional(), null, global.document);
-  const container = document.querySelector('.container')
-  container.appendChild(element);
+  global.window.onload = () => {
+    const element = renderElement(constitutional(), null, global.document);
+    const container = document.querySelector('#container')
+    container.appendChild(element);
+  };
 }
 
 export default constitutional;
